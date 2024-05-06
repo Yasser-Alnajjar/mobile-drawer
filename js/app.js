d55441347bc69796b93358bf0ebd8577be2456be
Yasser Al-Najjar <yasseralnajjar@outlook.sa>
2024-05-06 10:57:26 +0300

    add files

--- js/app.js
+++ js/app.js
@@ -2,16 +2,29 @@ document.body.addEventListener("touchstart", handleTouchStart);
 document.body.addEventListener("touchmove", handleTouchMove);
 document.body.addEventListener("touchend", handleTouchEnd);
 document.body.addEventListener("touchcancel", handleTouchCancel);
-
+const drag = document.querySelector(".drag");
+window.addEventListener("mousemove", (e) => {
+  console.log(e.clientX);
+  drag.style.top = e.clientY + "px";
+  drag.style.left = e.clientX + "px";
+});
 function handleTouchStart(e) {
-  console.log("TouchStart");
+  console.log(e.clientX);
+  drag.style.top = e.clientY + "px";
+  drag.style.left = e.clientX + "px";
 }
 function handleTouchMove(e) {
-  console.log("TouchMove");
+  console.log(e.clientX);
+  drag.style.top = e.clientY + "px";
+  drag.style.left = e.clientX + "px";
 }
 function handleTouchEnd(e) {
-  console.log("TouchEnd");
+  console.log(e.clientX);
+  drag.style.top = e.clientY + "px";
+  drag.style.left = e.clientX + "px";
 }
 function handleTouchCancel(e) {
-  console.log("TouchCancel");
+  console.log(e.clientX);
+  drag.style.top = 0;
+  drag.style.left = 0;
 }
